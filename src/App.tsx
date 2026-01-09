b904a669c77b6ddd74f03b8332efe559d95e4939
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2026-01-09 16:59:03 +0300

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import NotFound from "./pages/not-found";
+ 
 
 const App: FC = () => {
   return (
